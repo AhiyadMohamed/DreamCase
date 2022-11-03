3ac4e8136b9f8bc10c0874500e1ab9920a69ea4e
AhiyadMohamed <medahyad2017@gmail.com>
2022-11-03 14:10:57 +0100

    First Commit

---
+++ src/test/java/fr/inventiv/mahiyad/dreamcase/DreamCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.inventiv.mahiyad.dreamcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
